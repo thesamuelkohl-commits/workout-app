1f78fbdad9f4671c37d6cfb61d0d1f78f2c6586f
thesamuelkohl-commits <thesamuelkohl@gmail.com>
2026-07-04 13:22:01 -0500

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,43 @@
+const CACHE = 'workout-tracker-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './css/style.css',
+  './js/db.js',
+  './js/app.js',
+  './manifest.json',
+  './icons/icon-192.png',
+  './icons/icon-512.png'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE).then((cache) => cache.addAll(ASSETS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((k) => k !== CACHE).map((k) => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (event) => {
+  if (event.request.method !== 'GET') return;
+  event.respondWith(
+    caches.match(event.request).then((cached) => {
+      if (cached) return cached;
+      return fetch(event.request)
+        .then((res) => {
+          const copy = res.clone();
+          caches.open(CACHE).then((cache) => cache.put(event.request, copy));
+          return res;
+        })
+        .catch(() => cached);
+    })
+  );
+});
